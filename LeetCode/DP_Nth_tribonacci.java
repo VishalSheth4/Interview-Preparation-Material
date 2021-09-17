2800f144785b2b4e03fbcc79c41d2462f42ec1ea
VishalSheth4 <vishalsheth4@gmail.com>
2021-09-17 14:24:20 +0530

    Update DP_Nth_tribonacci.java

--- LeetCode/DP_Nth_tribonacci.java
+++ LeetCode/DP_Nth_tribonacci.java
@@ -1,17 +1,27 @@
 class Solution {
-    public int tribonacci(int n) {
-        if(n<0){
-            return 0;
-        }
+    public  int triUtil(int a[], int n){
         if(n==0){
             return 0;
-        }
-        if(n==1){
+        }else if(n==1){
             return 1;
-        }
-        if(n==2){
+        }else if(n==2){
             return 1;
+        }else if(a[n] != -1){
+            return a[n];
+        }else{
+            a[n] = triUtil(a, n-3) + triUtil(a, n-1) + triUtil(a, n-2);
+            return a[n];
         }
-        return tribonacci(n-3) + tribonacci(n-1) + tribonacci(n-2);
+    }
+    public int tribonacci(int n) {
+        if(n <= 0){
+            return 0;
+        }
+        n=n+1;
+        int a[] = new int[n];
+        for(int i=0;i<n;i++){
+            a[i] = -1;
+        }
+        return triUtil(a,--n);
     }
 }
