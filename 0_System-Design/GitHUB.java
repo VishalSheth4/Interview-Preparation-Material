39ca5770e431a102e427ae021acd7af0f18e515d
VishalSheth4 <vishalsheth4@gmail.com>
2021-09-22 02:18:51 +0530

    Create GitHUB.java

---
+++ 0_System-Design/GitHUB.java
@@ -0,0 +1,16 @@
+GITHUB -----
+  
+ /*
+ GIT - Version Control
+ system that records to a set of files and help us recall specific version later needed
+ Access :
+   Revert files or whole project to an earlier state.
+   Compare changes over time
+   see who modified
+   control modifications by collaborators with the permission of admin/owners.
+   
+ Github 
+  Repository Hosting service for git
+   
+   
+   https://www.oreilly.com/library/view/version-control-with/9781449345037/ch04.html
