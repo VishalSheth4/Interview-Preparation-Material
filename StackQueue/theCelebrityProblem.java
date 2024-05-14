4bd66b692227a7a3574d9b8fd4686b4bfc49ebae
vishal <vishalsheth4@gmail.com>
2024-05-14 05:42:12 +0530

    The celebrity problem

--- StackQueue/theCelebrityProblem.java
+++ StackQueue/theCelebrityProblem.java
@@ -1,40 +1,61 @@
-package StackQueue;
+import java.util.Stack;
 
-import java.util.*;
-public class theCelebrityProblem {
-	static int MATRIX[][] = { 
-			{ 0, 0, 1, 0 },
-            { 0, 0, 1, 0 },
-            { 0, 0, 0, 0 },
-            { 0, 0, 1, 0 } };
-	
-	public static int knows(int first, int second) {
-		if(MATRIX[first][second]==1) {
-			return second;
-		}else {
-			return first;
-		}
-	}
-	public static void main(String args[]) {
-		Stack<Integer> st = new Stack<>();
-		int n=MATRIX.length;
-		for(int i=0;i<n;i++) {
-			st.push(i);
-		}
-		while(st.size()>1) {
-			int first=st.pop();
-			int second=st.pop();
-			int addTemp=knows(first,second);
-			st.push(addTemp);
-		}
-		int c=st.pop();
-		System.out.print(c);
-//		for (int i = 0; i < n; i++) {
-//            // If any person doesn't know 'c' or 'a' doesn't
-//            // know any person, return -1
-//            if (i != c && (knows(c, i) || (!knows(i, c)))) {
-//            	
-//            }
-//        }
-	}
+public class CelebrityProblem {
+    // A utility function to check if a knows b
+    static boolean knows(int a, int b, boolean[][] knowledge) {
+        return knowledge[a][b]; // Return true if a knows b, false otherwise
+    }
+
+    // Returns the celebrity's ID if celebrity exists, -1 otherwise
+    static int findCelebrity(int n, boolean[][] knowledge) {
+        Stack<Integer> stack = new Stack<>();
+
+        // Push all people onto stack
+        for (int i = 0; i < n; i++) {
+            stack.push(i);
+        }
+
+        // While there are more than one person in the stack
+        while (stack.size() > 1) {
+            int a = stack.pop();
+            int b = stack.pop();
+
+            if (knows(a, b, knowledge)) {
+                // If a knows b, 'a' cannot be the celebrity, push 'b' back
+                stack.push(b);
+            } else {
+                // If a doesn't know b, 'b' cannot be the celebrity, push 'a' back
+                stack.push(a);
+            }
+        }
+
+        // Potential celebrity ID
+        int potentialCelebrity = stack.pop();
+
+        // Check if potentialCelebrity is actually the celebrity
+        for (int i = 0; i < n; i++) {
+            if (i != potentialCelebrity && (knows(potentialCelebrity, i, knowledge) || !knows(i, potentialCelebrity, knowledge))) {
+                return -1; // Potential celebrity knows someone or someone doesn't know the potential celebrity
+            }
+        }
+
+        return potentialCelebrity;
+    }
+
+    public static void main(String[] args) {
+        int n = 4; // Number of people at the party
+        boolean[][] knowledge = {
+            {false, true, false, true},
+            {false, false, true, false},
+            {true, false, false, true},
+            {false, false, false, false}
+        };
+
+        int celebrity = findCelebrity(n, knowledge);
+        if (celebrity == -1) {
+            System.out.println("No celebrity found!");
+        } else {
+            System.out.println("Celebrity found with ID: " + celebrity);
+        }
+    }
 }
