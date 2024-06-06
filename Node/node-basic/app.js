7dd64c1795f04c67fe4c3d45901ed5064b1692d0
vishal <vishalsheth4@gmail.com>
2024-06-06 10:49:58 +0530

    Learn about basic node.js - Arrow function and import and export functionality.

--- Node/node-basic/app.js
+++ Node/node-basic/app.js
@@ -12,9 +12,30 @@ console.log("Total: ",total);
 
 // export sub function from helper.js
 
-console.log("Process",process);
+// console.log("Process",process);
 
 const helpers = require('./helpers')
 
 const subTotal = helpers.sub(5,1);
 console.log("SubTotal: ",subTotal);
+
+// ------- Arrow function
+const multiplyTotal = helpers.multiply(2,3)
+console.log("Multiply", multiplyTotal);
+
+
+const divide = helpers.divide(10,2)
+console.log("divide", divide);
+
+// ------- Object destructuring //
+
+const {sub} = require('./helpers1')
+console.log("sub", sub(9,8));
+
+// ---- node js core modules.
+const http = require('http')
+const server = http.createServer((req, res) => {
+    res.end("Hello world from node.js")
+})
+
+server.listen(3000);
